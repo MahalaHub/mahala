1bc2dad5dfdba71120bab505e6fb993aea3edba8
semirmahovkic <semirmahovkic@192.168.1.15>
2023-08-23 10:26:03 +0200

    login

--- app/src/main/java/semir/mahovkic/mahalahub/ui/login/LoginScreen.kt
+++ app/src/main/java/semir/mahovkic/mahalahub/ui/login/LoginScreen.kt
@@ -13,6 +13,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -31,92 +32,136 @@ fun LoginScreen(
 ) {
     val uiState: LoginUiState by viewModel.uiState.collectAsStateWithLifecycle()
     val searchBy = remember { mutableStateOf(EmptySearchBy) }
-    val username = remember { mutableStateOf("") }
-    val emailOrPhoneNumber = remember { mutableStateOf("") }
 
     Box(
-        modifier = Modifier
-            .fillMaxSize()
+        modifier = Modifier.fillMaxSize()
     ) {
-        Column(
+        LogoHeader(
             modifier = Modifier
                 .padding(50.dp)
                 .align(Alignment.TopCenter)
-        ) {
-            LogoImage(
-                Modifier
-            )
+        )
 
-            Text(
-                text = "Mahala!",
-                style = MaterialTheme.typography.titleLarge,
-                fontWeight = FontWeight.Bold,
-                modifier = Modifier
-                    .padding(8.dp)
-                    .align(Alignment.CenterHorizontally)
-            )
-        }
-
-        Column(
+        LoginForm(
             modifier = Modifier
                 .padding(start = 40.dp, end = 40.dp, top = 100.dp)
-                .align(Alignment.Center)
-        ) {
-            OutlinedTextField(
-                value = username.value,
-                onValueChange = { username.value = it },
-                shape = RoundedCornerShape(30.dp),
-                singleLine = true,
-                placeholder = {
-                    Text(
-                        text = "Izaberi korisničko ime",
-                        color = MaterialTheme.colorScheme.outline,
-                        style = MaterialTheme.typography.titleMedium
-                    )
-                },
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .align(Alignment.CenterHorizontally),
-                textStyle = MaterialTheme.typography.titleMedium,
+                .align(Alignment.Center),
+            navigateToHome
+        )
+    }
+}
+
+@Composable
+fun LogoHeader(modifier: Modifier) {
+    Column(
+        modifier = modifier
+    ) {
+        LogoImage(Modifier)
+
+        Text(
+            text = "Mahala!",
+            style = MaterialTheme.typography.titleLarge,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier
+                .padding(8.dp)
+                .align(Alignment.CenterHorizontally)
+        )
+    }
+}
+
+@Composable
+fun LoginForm(modifier: Modifier, onClick: () -> Unit) {
+    val username = remember { mutableStateOf("") }
+    val emailOrPhoneNumber = remember { mutableStateOf("") }
+
+    Column(
+        modifier = modifier
+    ) {
+        UsernameField(
+            username,
+            modifier = Modifier
+                .fillMaxWidth()
+                .align(Alignment.CenterHorizontally)
+        )
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+        EmailField(
+            emailOrPhoneNumber,
+            modifier = Modifier
+                .fillMaxWidth()
+                .align(Alignment.CenterHorizontally)
+        )
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+        LoginButton(
+            username.value,
+            emailOrPhoneNumber.value,
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally),
+            onClick
+        )
+    }
+}
+
+@Composable
+fun UsernameField(username: MutableState<String>, modifier: Modifier) {
+    OutlinedTextField(
+        value = username.value,
+        onValueChange = { username.value = it },
+        shape = RoundedCornerShape(30.dp),
+        singleLine = true,
+        placeholder = {
+            Text(
+                text = "Izaberi korisničko ime",
+                color = MaterialTheme.colorScheme.outline,
+                style = MaterialTheme.typography.titleMedium
             )
+        },
+        textStyle = MaterialTheme.typography.titleMedium,
+        modifier = modifier
+    )
+}
 
-            Spacer(modifier = Modifier.height(20.dp))
-
-            OutlinedTextField(
-                value = emailOrPhoneNumber.value,
-                onValueChange = { emailOrPhoneNumber.value = it },
-                shape = RoundedCornerShape(30.dp),
-                singleLine = true,
-                placeholder = {
-                    Text(
-                        text = "Broj telefona ili email adresa",
-                        color = MaterialTheme.colorScheme.outline,
-                        style = MaterialTheme.typography.titleMedium
-                    )
-                },
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .align(Alignment.CenterHorizontally),
-                textStyle = MaterialTheme.typography.titleLarge,
+@Composable
+fun EmailField(emailOrPhoneNumber: MutableState<String>, modifier: Modifier) {
+    OutlinedTextField(
+        value = emailOrPhoneNumber.value,
+        onValueChange = { emailOrPhoneNumber.value = it },
+        shape = RoundedCornerShape(30.dp),
+        singleLine = true,
+        placeholder = {
+            Text(
+                text = "Broj telefona ili email adresa",
+                color = MaterialTheme.colorScheme.outline,
+                style = MaterialTheme.typography.titleMedium
             )
+        },
+        textStyle = MaterialTheme.typography.titleMedium,
+        modifier = modifier
+    )
+}
 
-            Spacer(modifier = Modifier.height(20.dp))
+@Composable
+fun LoginButton(
+    username: String,
+    emailOrPhoneNumber: String,
+    modifier: Modifier,
+    onClick: () -> Unit
+) {
+    val loginEnabled =
+        username.trim().isNotEmpty() && emailOrPhoneNumber.trim().isNotEmpty()
 
-            val loginEnabled =
-                username.value.trim().isNotEmpty() && emailOrPhoneNumber.value.trim().isNotEmpty()
-
-            Button(
-                enabled = loginEnabled,
-                modifier = Modifier
-                    .align(Alignment.CenterHorizontally),
-                onClick = navigateToHome
-            ) {
-                Text(
-                    modifier = Modifier.padding(8.dp),
-                    text = "Login",
-                    style = MaterialTheme.typography.titleLarge
-                )
-            }
-        }
+    Button(
+        enabled = loginEnabled,
+        modifier = modifier,
+        onClick = onClick
+    ) {
+        Text(
+            modifier = Modifier.padding(8.dp),
+            text = "Login",
+            style = MaterialTheme.typography.titleLarge
+        )
     }
 }
